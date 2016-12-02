03d998cbd7ce630d9ac0013d024d279c434c5f3d
revooms <revooms@users.noreply.github.com>
2016-12-02 12:58:09 +0100

    Added professions

--- src/Profession.php
+++ src/Profession.php
@@ -2,15 +2,69 @@
 
 class Profession extends \Faker\Provider\Base
 {
+    /**
+     * @var array
+     *
+     * See https://en.wikipedia.org/wiki/List_of_professions
+     */
     protected static $professionList = array(
-        'Butcher',
+        'Accountant',
+        'Air traffic controller',
+        'Aircraft pilot',
+        'Anesthesiologist',
+        'Architect',
+        'Astronomer',
+        'Audiologist',
+        'Biologist',
+        'Botanist',
+        'Chemist',
+        'Chiropractor',
+        'Clergy',
+        'Dentist',
+        'Ecologist',
+        'Engineer',
+        'Geneticist',
+        'Geologist',
+        'Health inspector',
+        'Immunologist',
+        'Interpreter',
         'Lawyer',
-        'Librarian'
+        'Meteorologist',
+        'Military officer',
+        'Nurse',
+        'Oceanographer',
+        'Optometrist',
+        'Paleontologist',
+        'Park ranger',
+        'Pharmacist',
+        'Pharmacologist',
+        'Philosopher',
+        'Physician',
+        'Physicist',
+        'Podiatrist',
+        'Police officer',
+        'Professor',
+        'Psychologist',
+        'Radiographer',
+        'Radiotherapist',
+        'Scientist',
+        'Sea captain',
+        'Social Worker',
+        'Surgeon',
+        'Surveyor',
+        'Teacher',
+        'Translator',
+        'Urban Planner',
+        'Veterinarian',
+        'Virologist',
+        'Zoologist',
+
     );
 
     public function profession()
     {
         $format = static::randomElement(static::$professionList);
+
         return $this->generator->parse($format);
     }
 
